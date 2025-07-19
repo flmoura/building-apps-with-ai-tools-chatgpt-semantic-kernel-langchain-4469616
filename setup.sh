d7fbcfbee11c4cb7b13250ee0325f6d72c41af0c
Flavio Moura <flavio.moura@gmail.com>
2025-07-19 21:26:55 +0000

    02_01b

---
+++ setup.sh
@@ -0,0 +1,4 @@
+cd src
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
--- src/sentiment_classifier.py
+++ src/sentiment_classifier.py
@@ -4,15 +4,22 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-response = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=[
-        {"role": "system", "content": ""},
-        {"role": "user", "content": ""}
-    ],
-    temperature=0.7,
-    max_tokens=150,
-)
+while True:
+    user_input = input("Enter a phrase and we'll tell you if its happy or sad: ")
 
-response_message = response["choices"][0]["message"]
-print(response_message)
+    if user_input == "exit" or user_input == "quit":
+        break
+
+    response = openai.ChatCompletion.create(
+        # model="gpt-3.5-turbo",
+        model="gpt-4.1-nano",
+        messages=[
+            {"role": "system", "content": "You are a sentiment classification bot, print out if the user is happy or sad."},
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
+
+    response_message = response["choices"][0]["message"]
+    print(response_message)
