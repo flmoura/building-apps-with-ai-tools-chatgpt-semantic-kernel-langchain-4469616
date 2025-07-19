ea1a9aa334e1608427d91c0c92d2293ac0f4fcba
Flavio Moura <flavio.moura@gmail.com>
2025-07-19 22:06:17 +0000

    02_03

--- src/rude_customer_detector.py
+++ src/rude_customer_detector.py
@@ -12,3 +12,34 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 # test case 3 'I like pizza. What do you like?'
 # test case 4 'I bite my thumb at you!'
 # test case 5 'I think this product doesnt work!' -> RUDE
+
+
+system_prompt = """You are a sentiment classification bot.
+Classify user's question as 'RUDE' or 'KIND'.
+If it is a KIND question, answer it.
+If it is a RUDE question, just return 'RUDE'.
+"""
+
+while True:
+  user_input = input("Hello! What's your question? ")
+
+  if user_input.lower().strip() in ["exit", "quit"]:
+    break
+
+  response = openai.ChatCompletion.create(
+    model="gpt-4.1-nano",
+    messages=[
+      {"role": "system", "content": system_prompt},
+      {"role": "user", "content": user_input}
+    ],
+    temperature=0.7,
+    max_tokens=150,
+  )
+
+  response_message = response["choices"][0]["message"]
+
+  if response_message.get("content").upper().strip() == "RUDE":
+    print("You are being RUDE. Goodbye!")
+    break
+
+  print(response_message)
