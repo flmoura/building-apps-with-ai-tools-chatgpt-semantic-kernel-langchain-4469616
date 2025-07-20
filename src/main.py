6cf6f409c7afba5aee58ce1f0d7855da02ce2b4c
Flavio Moura <flavio.moura@gmail.com>
2025-07-20 01:22:24 +0000

    04_01b

--- src/main.py
+++ src/main.py
@@ -1,1 +1,46 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate
+)
+from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
+
+class CommaSeparatedListOutputParser(BaseOutputParser):
+  """Parse the output of an LLM call to a comma-separated list."""
+
+  def parse(self, text: str):
+    """Parse the output of an LLM call."""
+    return text.strip().split(", ")
+
+chat_model = ChatOpenAI()
+
+# print(chat_model.predict("Hi!"))
+
+system_template = """You are a helpful assistant who generates comma separated lists.
+A user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
+ONLY return a comma separated list, and nothing more.
+"""
+system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+human_template = "{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([
+  system_message_prompt,
+  human_message_prompt
+])
+
+chain = LLMChain(
+  llm=ChatOpenAI(),
+  prompt=chat_prompt,
+  output_parser=CommaSeparatedListOutputParser()
+)
+
+# blue, green, yellow, red, purple
+print(chain.run("colours"))
+
+print(chain.run("Cities"))
+
+print(chain.run("best food in the world"))
